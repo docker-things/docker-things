b072a1e189ccc9cdfe964927c131682f2f97c15d
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2019-09-04 15:29:38 +0300

    Set default config values

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Command used to launch docker
-DOCKER_CMD='docker'
+DOCKER_CMD="`which docker`"
 
 # Where to store the backups
-BACKUP_PATH=''
+BACKUP_PATH="$HOME/.docker-things/backups"
 
 # Where to store the things
-REPOS_PATH='$HOME/.docker-things'
+REPOS_PATH="$HOME/.docker-things/repos"
