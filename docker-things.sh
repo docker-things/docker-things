ee56c34b3702077b09236a99ad01e5d2e1a84df2
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2019-09-04 16:15:27 +0300

    Check own dependencies, manage thing dependencies, better handle repos

--- docker-things.sh
+++ docker-things.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEPENDENCIES="bash docker git"
+
 # WhereAmI function
 function get_script_dir() {
      SOURCE="${BASH_SOURCE[0]}"
@@ -23,29 +25,44 @@ function showYellow() { echo -e "\033[01;33m$@\033[00m"; }
 function showRed() { echo -e "\033[01;31m$@\033[00m"; }
 
 # Launch the required action
-function scriptRun() {
-    echo "${@:2}"
-    exit
-    for THING in "${@:2}"; do
-        case "$1" in
-            "build")        passCMD build $THING   ;;
-            "install")      passCMD install $THING ;;
-            "start")        passCMD start $THING   ;;
-            "stop")         passCMD stop $THING    ;;
-            "kill")         passCMD kill $THING    ;;
-            "get")          getThing $THING        ;;
-            "update")       updateThing $THING     ;;
-            "delete")       deleteThing $THING     ;;
-            "self-install") selfInstall            ;;
-            *)              showUsage              ;;
-        esac
-    done
+function main() {
+    checkDependencies
+    if [ $# -lt 2 ]; then
+        runCommand $@
+    else
+        for THING in ${@:2}; do
+            runCommand "$1" "$THING"
+        done
+    fi
+}
+function runCommand() {
+    case "$1" in
+        "build")        passCMD build $2   ;;
+        "install")      passCMD install $2 ;;
+        "start")        passCMD start $2   ;;
+        "stop")         passCMD stop $2    ;;
+        "kill")         passCMD kill $2    ;;
+        "get")          getThing $2        ;;
+        "update")       updateThing $2     ;;
+        "delete")       deleteThing $2     ;;
+        "self-install") selfInstall        ;;
+        *)              showUsage          ;;
+    esac
 }
 
 # Usage
 function showUsage() {
     showNormal "\nUsage: $0 [build|install|start|stop|kill|get|update|delete|self-install] [THING]\n"
     exit 1
+}
+
+function checkDependencies() {
+    for BIN in $DEPENDENCIES; do
+        if [ "`which $BIN`" == "" ]; then
+            showRed "[ERROR] Dependency not found: \"$BIN\""
+            exit
+        fi
+    done
 }
 
 # Self install
@@ -59,12 +76,19 @@ function selfInstall() {
      && cat \"$THIS_FILE\" | grep -v '. config.sh' >> $BIN_FILE \
      && chmod +x \"$BIN_FILE\" \
      "
+
+    showGreen "\nScript installed @ $BIN_FILE..."
 }
 
 # Passthrough calls
 function passCMD() {
-    showGreen "\n$1 $2..."
-    checkRepo "$REPOS_PATH/$2"
+    if [ "$1" == "build" ]; then
+        getThing "$2"
+    elif [ "$1" == "install" ]; then
+        passCMD build "$2"
+    fi
+    showGreen "\n${1^}ing $2..."
+    checkRepo "$2"
     cd "$REPOS_PATH/$2"
     bash docker.sh "$1"
 }
@@ -72,25 +96,42 @@ function passCMD() {
 # Repo related
 function getThing() {
     showGreen "\nGetting $1..."
-    if [ ! -d "$REPOS_PATH/$1/.git" ]; then
-        showRed "\n[ERROR][$1] Repository already exists!"
-        exit 1
+    if [ -d "$REPOS_PATH/$1/.git" ]; then
+        showNormal "\nRepository already exists!"
+    else
+        mkdir -p "$REPOS_PATH"
+        git clone "https://github.com/docker-things/$1.git" "$REPOS_PATH/$1"
+        checkRepo "$1"
     fi
-    mkdir -p "$REPOS_PATH"
-    git clone "https://github.com/docker-things/$1.git" "$REPOS_PATH/$1"
-    checkRepo "$REPOS_PATH/$1"
+    configThing "$1"
+    buildDependencies "$1"
 }
 function updateThing() {
     showGreen "\nUpdating $1..."
-    checkRepo "$REPOS_PATH/$1"
+    checkRepo "$1"
     cd "$REPOS_PATH/$1" && git checkout . && git pull
+    configThing "$1"
+    buildDependencies "$1"
 }
-# function configThing() {
-
-# }
 function deleteThing() {
     showGreen "\nDeleting $1..."
     rm -rf "$REPOS_PATH/$1"
+}
+function configThing() {
+    showGreen "\nConfiguring $1..."
+    cat "$REPOS_PATH/$1/config.sh" | grep -v 'DOCKER_CMD=' | grep -v 'BACKUP_PATH=' > "$REPOS_PATH/$1/config.sh.tmp"
+    echo "DOCKER_CMD='$DOCKER_CMD'" >> "$REPOS_PATH/$1/config.sh.tmp"
+    echo "BACKUP_PATH='$BACKUP_PATH'" >> "$REPOS_PATH/$1/config.sh.tmp"
+    rm -f "$REPOS_PATH/$1/config.sh"
+    mv "$REPOS_PATH/$1/config.sh.tmp" "$REPOS_PATH/$1/config.sh"
+}
+function buildDependencies() {
+    checkRepo "$1"
+    if [ -f "$REPOS_PATH/$1/.dependencies" ]; then
+        for DEPENDENCY in `cat "$REPOS_PATH/$1/.dependencies"`; do
+            passCMD build "$DEPENDENCY"
+        done
+    fi
 }
 
 # Checks
@@ -114,4 +155,4 @@ function checkRepo() {
 }
 
 # Actually do stuff
-scriptRun $@
+main $@
